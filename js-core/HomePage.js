4d0794f32507dec3d08d735196b8f599daa1011f
poberWong <poberwong@gmail.com>
2016-02-16 20:37:37 +0800

    add swiper and indicator

--- js-core/HomePage.js
+++ js-core/HomePage.js
@@ -1,30 +1,82 @@
 'use strict'
 import React from 'react-native'
-import Swiper from 'react-native-swiper'
+import Swiper from './swiper.js'
 
 const {
   StyleSheet,
+  Dimensions,
   Text,
   View
 } = React
 
-export default class HomePage extends React.Component{
+let screenWidth = Dimensions.get('window').width // 由pt转为px
+export default class HomePage extends React.Component {
+
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      leftFlex: 0,
+      colorPosition0: 'red',
+      colorPosition1: 'black'
+    }
+  }
+
+  onScroll (event) {
+    let offset = event.nativeEvent.contentOffset.x
+    let leftFlex = offset / screenWidth
+    let position0 = 'red'
+    let position1 = 'black'
+    if (leftFlex === 0) {
+      position0 = 'red'
+      position1 = 'black'
+    } else if (leftFlex === 1) {
+      position0 = 'black'
+      position1 = 'red'
+    }
+
+    this.setState({
+      leftFlex,
+      colorPosition0: position0,
+      colorPosition1: position1
+    })
+  }
+
   render () {
     return (
       <View style={styles.container}>
         <View style={styles.content}>
-        <View style={styles.slidingIndicatorWrapper}>
+          <View style={styles.slidingIndicatorWrapper}>
+            <View style={styles.indicatorItems}>
+              <View style={styles.indicatorItem}>
+                <Text style={{color: this.state.colorPosition0}}>妹子</Text>
+              </View>
+              <View style={styles.indicatorItem}>
+                <Text style={{color: this.state.colorPosition1}}>汉子</Text>
+              </View>
+            </View>
+            <View style={styles.indicatorWrapper}>
+              <View style={{flex: this.state.leftFlex}}/>
+              <View style={styles.indicator}/>
+              <View style={{flex: 1 - this.state.leftFlex}}/>
+            </View>
+          </View>
 
+          <Swiper
+            key='swiper'
+            onScroll ={this.onScroll.bind(this)}
+            scrollEventThrottle ={10}
+            onMomentumScrollEnd ={this._onMomentumScrollEnd}
+            loop={false}>
+
+            <View style={styles.slide1}>
+              <Text style={styles.text}>Hello Swiper</Text>
+            </View>
+            <View style={styles.slide2}>
+              <Text style={styles.text}>Beautiful</Text>
+            </View>
+          </Swiper>
         </View>
-
-        <Swiper style={styles.wrapper}>
-          <View style={styles.slide1}>
-            <Text style={styles.text}>Hello Swiper</Text>
-          </View>
-          <View style={styles.slide2}>
-            <Text style={styles.text}>Beautiful</Text>
-          </View>
-        </Swiper>
       </View>
     )
   }
@@ -35,13 +87,30 @@ let styles = StyleSheet.create({
     flex: 1
   },
   content: {
-    paddingLeft: 10,
-    paddingRight: 10
+    marginTop: 64,
+    backgroundColor: '#eeeeee'
   },
   slidingIndicatorWrapper: {
     height: 40,
     flex: 1
-  }
+  },
+  indicatorItems: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  indicatorItem: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  indicatorWrapper: {
+    flexDirection: 'row',
+    height: 2
+  },
+  indicator: {
+    flex: 1,
+    backgroundColor: 'red'
+  },
   slide1: {
     flex: 1,
     justifyContent: 'center',
